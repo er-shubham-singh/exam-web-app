a257c646119e6e2eff51dae4c386e0ce763b5d05
er-shubham-singh <shubham.singh7985@gmail.com>
2025-09-26 16:45:29 +0530

    fixing smtp error

--- Backend/src/Config/email.config.js
+++ Backend/src/Config/email.config.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
--- exam/src/Component/CodingCard.jsx
+++ exam/src/Component/CodingCard.jsx
@@ -329,5 +329,4 @@ return (
     </section>
   </section>
 );
-
 }
